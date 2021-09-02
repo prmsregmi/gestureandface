d15d0d01fe1c20d46f734e2fd375c52d8629728a
Rewant <pokhrelrewant@gmail.com>
2021-09-02 20:34:38 +0545

    Code aggregation

---
+++ config.py
@@ -0,0 +1,6 @@
+process_this_frame = True
+face_match_threshold = 65 #percentage threshold for face match accuracy
+display_window = True #display image or not, will only display result when false
+display_bounding_box = True #display bound box or not, true is only valid when display_window is true
+display_hand_landmarks = True #display hand landmarks or not, true is only valid when display_window is true
+threshold = 200 #define pixel threshold between hand and face from 150 (close) to 300 (far)
---
+++ test.py
@@ -0,0 +1,5 @@
+my_obj = {
+    "name": "Rewant",
+    "gesture" : "Open"
+}
+print(my_obj)
